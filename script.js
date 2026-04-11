3171a3b3eab9da3b61e0b1ec144d23305d89ba82
vict0r213 <vm360650@gmail.com>
2026-04-11 16:08:41 -0300

    atualizando o script

--- script.js
+++ script.js
@@ -1,28 +1,18 @@
+console.log("Script carregado");
+
 const projetos = [
+
   {
-    nome: "Sistema de Pedidos Online",
-    descricao: "Aplicação para restaurantes gerenciarem pedidos.",
-    link: "#"
-  },
-  {
-    nome: "Controle de Estoque de Farmácia",
-    descricao: "Sistema para gerenciar produtos e vendas.",
-    link: "#"
-  },
-  {
-    nome: "App de Delivery",
-    descricao: "Plataforma simples de pedidos e entregas.",
-    link: "#"
+    nome: "SIGEC(Sistema de Gerenciamento Comercial)",
+    descricao: "Sistema para gerenciar produtos, vendas e estoque.",
+    link: "https://vict0r213.github.io/Sigec/"
   }
-];
 
 const container = document.getElementById("lista-projetos");
 
-projetos.forEach((projeto, index) => {
+projetos.forEach(projeto => {
   const div = document.createElement("div");
   div.classList.add("projeto");
-
-  div.style.animationDelay = `${index * 0.2}s`;
 
   div.innerHTML = `
     <h3>${projeto.nome}</h3>
